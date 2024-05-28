6bb3f7cee80f695d014851e80f448052d2624cb9
jesus doza <jesus.doza@gmail.com>
2024-05-28 10:53:42 -0500

    add rsa config to spring app entry point

--- src/main/java/com/jwt/JwtConfigApplication.java
+++ src/main/java/com/jwt/JwtConfigApplication.java
@@ -1,9 +1,12 @@
 package com.jwt;
 
+import com.jwt.configs.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class JwtConfigApplication {
 
 	public static void main(String[] args) {
